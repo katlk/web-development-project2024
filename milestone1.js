70880d7aee70f0d668d954a473343fc71608cb05
katherine <51919081+katlk@users.noreply.github.com>
2024-07-01 23:27:54 -0400

    Add files via upload

--- milestone1.js
+++ milestone1.js
@@ -1,8 +1,10 @@
+//Allows backend to get to receive user input.
 const name = document.getElementById('email')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 
 //Here the code checks for valid information to prevent a user from submitting nothing at all. I want the page to remail until the user submits actual text.
+//Used this link for help: https://www.youtube.com/watch?v=TCEgdiN0A8s
 form.addEventListener('submit', (e) => {
     let messages = []
     if (email.value === '' || email.value == null) {
@@ -13,6 +15,6 @@ form.addEventListener('submit', (e) => {
         e.preventDefault()
         errorElement.innerText = messages.join(', ')
     }
-
+//I did get stuck at this point with returning back to the homepage after a form is submitted.
     
 })
